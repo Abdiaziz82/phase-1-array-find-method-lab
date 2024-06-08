0b80da94ebdb71c6481f68f47bd41dde0bf71e25
Abdiaziz82 <abdiazizhared64@gmail.com>
2024-06-08 14:22:07 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+
+
+function superbowlWin(record) {
+    const winRecord = record.find(game => game.result === "W");
+    return winRecord ? winRecord.year : undefined;
+}
+
+
+const record = [
+    { year: "2018", result: "L" },
+    { year: "2019", result: "L" },
+    { year: "2020", result: "W" },
+    { year: "2021", result: "L" }
+];
+
+console.log(superbowlWin(record)); // Should print: 2020
+
